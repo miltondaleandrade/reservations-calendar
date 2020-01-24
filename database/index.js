fcaad9763b8bd2c27e9222161362bd3e5c9e9e99
robertnbritton <robertnbritton@gmail.com>
2020-01-24 11:28:00 -0800

    Create index.js to initialize database connection

--- database/index.js
+++ database/index.js
@@ -1,2 +1,14 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yelp-calendar')
+mongoose.connect('mongodb://localhost/yelp-calendar');
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('successfully connect to yelp-calendar db')
+});
+
+let calendarSchema = mongoose.Schema({
+  openHours: Object,
+  maxPartySize: Number
+});
+
+let Calendar = mongoose.model('Calendar', calendarSchema);
