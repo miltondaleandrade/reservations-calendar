d6d31ecb1f5ebb2dcaff83ba62903c5a986ffda1
robertnbritton <robertnbritton@gmail.com>
2020-01-24 14:10:58 -0800

    Complete index.js for server connection and schema setup

--- database/index.js
+++ database/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yelp-calendar');
 
+const db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('successfully connect to yelp-calendar db')
@@ -12,3 +14,5 @@ let calendarSchema = mongoose.Schema({
 });
 
 let Calendar = mongoose.model('Calendar', calendarSchema);
+
+module.exports.Calendar = Calendar;
