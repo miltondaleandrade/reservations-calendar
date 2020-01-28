84021cbc323b636a3dacc1032380d758a800d466
robertnbritton <robertnbritton@gmail.com>
2020-01-27 17:52:30 -0800

    Build queryDb function that is called on server GET request

--- database/index.js
+++ database/index.js
@@ -8,6 +8,8 @@ db.once('open', function() {
   console.log('successfully connect to yelp-calendar db')
 });
 
+
+// Define mongoDb Schema and Model
 let calendarSchema = mongoose.Schema({
   openHours: Object,
   maxPartySize: Number
@@ -15,4 +17,18 @@ let calendarSchema = mongoose.Schema({
 
 let Calendar = mongoose.model('Calendar', calendarSchema);
 
+
+// Define function to query db
+const queryDb = (callback) => {
+
+  Calendar.find({}, (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, data);    
+    }
+  });  
+};
+
 module.exports.Calendar = Calendar;
+module.exports.queryDb = queryDb;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+const db = require('../database/index.js')
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,14 @@ app.listen(port, () => { console.log(`Now listening on ${port}`) });
 
 // Build route handlers
 app.get('/', (req, res) => {
-  console.log('GET received');
-  res.send('hello world');
+
+  db.queryDb((err, data) => {
+    if (err) {
+      console.log('Error getting data from db ', err);
+      res.status(500).send(err);
+    } else {
+      console.log('Successful GET from db!');
+      res.status(200).send(data);
+    }
+  });
 })
